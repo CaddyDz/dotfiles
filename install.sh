1f9d3bf3ba03a3d6226ebd5c19d7b8a397225885
Salim Djerbouh <tbitw31@gmail.com>
2017-03-20 17:00:05 +0100

    Fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!bin/bash
-wget https://raw.githubusercontent.com/CaddyDz/dotfiles/master/src/debian/etc/sources.list && sudo rm /etc/apt/sources.list~ && sudo mv /home/caddy/sources.list /etc/apt/
+wget https://raw.githubusercontent.com/CaddyDz/dotfiles/master/src/debian/etc/sources.list && sudo rm /etc/apt/sources.list* && sudo mv /home/caddy/sources.list /etc/apt/
 # Update & upgrade the system
 sudo apt-get update && sudo apt-get upgrade
 # Install git version control
