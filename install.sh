439b57ea6898d1a306493b04f97735a0f5681bf3
Caddy Dz <tbitw31@gmail.com>
2017-09-19 16:41:55 +0100

    Remove Facebook style
    Signed-off-by: Caddy Dz <tbitw31@gmail.com>

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 sudo wget https://raw.githubusercontent.com/CaddyDz/dotfiles/master/src/debian/etc/sources.list -P /etc/apt/
 # Update & upgrade the system
 sudo apt-get update && sudo apt-get upgrade
+# Upgrade the Distro to the latest debian stretch
 # Install git version control
 sudo apt-get install git
 # Configure git
