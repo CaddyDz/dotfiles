812f6eb5fca1687d5808770459cf416cd0379e68
Salim Djerbouh <caddydz4@gmail.com>
2020-12-26 02:55:26 +0100

    Added eslint config for NuxtJS
    Signed-off-by: Salim Djerbouh <caddydz4@gmail.com>

--- node/.eslintrc.js
+++ node/.eslintrc.js
@@ -1,21 +1,37 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es6": true
+	root: true,
+	env: {
+		browser: true,
+		node: true
 	},
-	"extends": [
-		"plugin:vue/essential",
-		"standard"
-	],
-	"globals": {
-		"Atomics": "readonly",
-		"SharedArrayBuffer": "readonly"
+	parserOptions: {
+		parser: 'babel-eslint'
 	},
-	"parserOptions": {
-		"ecmaVersion": 2018,
-		"sourceType": "module"
-	},
-	"plugins": [
-		"vue"
-	]
-};
+	extends: [],
+	plugins: [],
+	// add your custom rules here
+	rules: {
+		indent: ['warn', 'tab'],
+		'no-tabs': 0,
+		'vue/html-closing-bracket-newline': ['error', {
+			multiline: 'never'
+		}],
+		'vue/max-attributes-per-line': 'off',
+		'vue/html-indent': ['error', type, {
+			attribute: 20,
+			baseIndent: 1,
+			closeBracket: 1,
+			alignAttributesVertically: false,
+			ignores: []
+		}],
+		'vue/html-self-closing': ['error', {
+			html: {
+				void: 'never',
+				normal: 'always',
+				component: 'always'
+			},
+			svg: 'always',
+			math: 'always'
+		}]
+	}
+}
