d9235c86269013b919bb9066308f26604ed33100
Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>
2023-06-12 19:52:20 +0000

    Override gp alias to auto set an upstream (#18)
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- .functions.sh
+++ .functions.sh
@@ -14,3 +14,20 @@ function splitCsv() {
 		sed -i -e "1i$HEADER" "$i"
 	done
 }
+
+# Remove oh-my-zsh alias
+unalias gp
+# Register a global function
+function gp() {
+  # Get the current branch name
+  branch=$(git rev-parse --abbrev-ref HEAD)
+
+  # Check if the upstream branch is already set
+  if ! git rev-parse --abbrev-ref "@{u}" >/dev/null 2>&1; then
+    # Set the upstream branch
+    git push --set-upstream origin "$branch"
+  fi
+
+  # Push the current branch
+  git push
+}
