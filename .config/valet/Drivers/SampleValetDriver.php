938599f51352abfec4ac5296f61553fd1561f45e
Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>
2024-05-12 12:08:52 +0100

    Using Valet (#50)
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

---
+++ .config/valet/Drivers/SampleValetDriver.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Valet\Drivers\Custom;
+
+use Valet\Drivers\ValetDriver;
+
+class SampleValetDriver extends ValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     */
+    public function serves(string $sitePath, string $siteName, string $uri): bool
+    {
+        // if (file_exists($sitePath.'/file-that-identifies-my-framework')) {
+        //     return true;
+        // }
+
+        return false;
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     */
+    public function isStaticFile(string $sitePath, string $siteName, string $uri)/*: string|false */
+    {
+        if (file_exists($staticFilePath = $sitePath.'/public/'.$uri)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     */
+    public function frontControllerPath(string $sitePath, string $siteName, string $uri): string
+    {
+        return $sitePath.'/public/index.php';
+    }
+}
